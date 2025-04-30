e8deed7f8b67b2d0d0e7bc7e595636e398856c48
Simone Pagliuca <pagliuca.simone01@gmail.com>
2025-04-30 09:31:08 +0200

    Fix empty export
    the export command would previously return a cvs of only zeros

--- COTRA/export.py
+++ COTRA/export.py
@@ -7,10 +7,13 @@ def save_profile(file_name, point, domain='space'):
     if domain == 'space':
         index = np.argmin(np.abs(Grid_Space - point))
         concentration = C_full[index, :]
+        np.savetxt("Numerical_Evaluation.csv", np.column_stack((t_full, concentration)), delimiter=",")
+        
     elif domain == 'time':
         index = np.argmin(np.abs(t_full - point))
-        concentration = C_full[:, index]
+        concentration = C_full[:, index]       
+        np.savetxt("Numerical_Evaluation.csv", np.column_stack((Grid_Space, concentration)), delimiter=",")
+        
     else:
         raise ValueError("Domain must be 'space' or 'time'")
 
-    np.savetxt("Numerical_Evaluation.csv", concentration, delimiter=",")
